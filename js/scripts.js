0063d248088ffc4d3b0fb4bdf30f76c8557998b5
davidabutler92 <davidabutler92@yahoo.com>
2020-03-26 17:56:38 -0700

    add changed everything to check boxes and togglefade info

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
-  
-
+  $('input#eagle').click(function() {
+    $('#eagle-info').fadeToggle();
+  });
+  $('input#snake').click(function() {
+    $('#snake-info').fadeToggle();
+  });
+  $('input#elephant').click(function() {
+    $('#elephant-info').fadeToggle();
+  });
 });
